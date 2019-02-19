e3a05586d688c071f9680e027a3e2d67492a5062
zhoutingjie <zhoutjcc@msn.cn>
2019-02-19 11:28:02 +0800

    添加初始化构建

--- android/src/cc/fotoplace/AndroidLauncher.java
+++ android/src/cc/fotoplace/AndroidLauncher.java
@@ -1,16 +1,37 @@
 package cc.fotoplace;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import cc.fotoplace.MyGdxGame;
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
 
-public class AndroidLauncher extends AndroidApplication {
+import cc.fotoplace.spine.LibgdxSpineFragment;
+
+public class AndroidLauncher extends FragmentActivity implements AndroidFragmentApplication.Callbacks{
+	private LibgdxSpineFragment m_libgdxFgm;
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.spine_activity);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		m_libgdxFgm = (LibgdxSpineFragment) getSupportFragmentManager().findFragmentById(R.id.libgdxFrag);
+	}
+
+
+	@Override
+	public void finish() {
+		m_libgdxFgm.preDestory();
+		super.finish();
+	}
+
+	@Override
+	public void exit() {
+
+	}
+
+	@Override
+	public void onPointerCaptureChanged(boolean hasCapture) {
+
 	}
 }
